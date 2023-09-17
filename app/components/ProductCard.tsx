8989ac3e246ef3ba3af95e417aebaa9ece71a20c
Doniyor Alimov <alimov.doniyor28@gmail.com>
2023-09-17 08:07:59 +0500

    Tailwind CSS

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import AddToCart from './AddToCart'
+import React from "react";
+import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
-    <div>
-        <AddToCart />
+    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500">
+      <AddToCart />
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
