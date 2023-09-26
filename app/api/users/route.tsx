d77106222ccb91955a7f3eb41ce0de6d258fd69e
Doniyor Alimov <alimov.doniyor28@gmail.com>
2023-09-26 14:28:41 +0500

    Creating Data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -12,8 +12,22 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
 
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  return NextResponse.json(newUser, { status: 201 });
 }
